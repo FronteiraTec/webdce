8d6bdba31cb6fd46ef7a540e5911a7184f38711a
Fabricio Zanotto <fabricioz.1.2@hotmail.com>
2020-08-10 19:08:43 -0300

    mudando contato.php para incluir header.php nele

--- contato.php
+++ contato.php
@@ -15,66 +15,44 @@
     <link href="css/contato.css" rel="stylesheet">
     <link rel="stylesheet" href="css/bulma.min.css">
     <style>
-        body {
-            font-family: Arial, Helvetica, sans-serif;
+    body {
+        font-family: Arial, Helvetica, sans-serif;
 
-        }
+    }
 
-        .content {
-            display: flex;
-            justify-content: center;
-            margin-top: 100px;
-        }
+    .content {
+        display: flex;
+        justify-content: center;
+        margin-top: 0px;
+    }
 
-        .contato {
-            width: 100%;
-            max-width: 500px;
-        }
+    .contato {
+        width: 100%;
+        max-width: 500px;
+    }
 
-        .form {
-            display: flex;
-            flex-direction: column;
-        }
+    .form {
+        display: flex;
+        flex-direction: column;
+    }
 
-        .field {
-            padding: 10px;
-            margin-bottom: 15px;
-            border: 1px solid #DDD;
-            border-radius: 5px;
-            font-family: Arial, Helvetica, sans-serif;
-        }
+    .field {
+        padding: 10px;
+        margin-bottom: 15px;
+        border: 1px solid #DDD;
+        border-radius: 5px;
+        font-family: Arial, Helvetica, sans-serif;
+    }
 
-        textarea {
-            height: 150px;
-        }
+    textarea {
+        height: 150px;
+    }
     </style>
 </head>
 
 <body>
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <div class="row">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
-                        <i class="fa fa-bars"></i>
-                    </button>
-                    <a href="index.html"><img src="imagens/dce_logo.png" width="120" alt="logo-dce"></a>
-                </div>
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="menu">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="noticias.html">Noticias</a></li>
-                        <li><a href="entidades.html">Entidades</a></li>
-                        <li><a href="documentos.html">Documentos</a></li>
-                        <li><a href="sobre.html">Sobre</a></li>
-                        <li><a href="contato.html">Contato</a></li>
-                    </ul>
-                </div>
-                <!-- /.Navbar-collapse -->
-            </div>
-        </div>
-    </nav>
+    <?php require_once("includes/header.php"); ?>
+
 
     <section class="content">
         <div class="contato">
@@ -93,4 +71,5 @@
 
 
 </body>
+
 </html>
