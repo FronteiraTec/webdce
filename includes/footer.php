b38fd65aab72cfc6502e5531003a9bd0215921ec
mazarafa <arruda.rafa.ra@gmail.com>
2020-08-10 21:59:02 -0400

    alterações feitas no footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,16 @@
-    <!-- Footer -->
+    <div class="localizacao">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d113646.78846818066!2d-52.732016350498185!3d-27.11025028476605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94fb4b2bbbd96a4b%3A0x5871f4d6809cb67e!2sUniversidade%20Federal%20da%20Fronteira%20Sul%2C%20Campus%20Chapec%C3%B3!5e0!3m2!1spt-BR!2sbr!4v1597082565539!5m2!1spt-BR!2sbr" width="600" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+
+
+
+
     <footer>
-        <div class="footer-copyright text-center" id="rodapeinicial">
-            <p>Forte Boi Indústria de Alimentos LTDA</p>
-        </div>
+        <br><br>
+        <p>© Copyright 2020</p>
+
     </footer>
+
 
 
 
@@ -11,6 +18,5 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-    </body>
-
-    </html>
+</body>
+</html>
