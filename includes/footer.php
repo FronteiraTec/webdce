c382d046fbb6d9c85dcf0de4d6a31583dfa0a4fd
Fabricio Zanotto <fabricioz.1.2@hotmail.com>
2020-08-11 15:25:19 -0300

    configurando e add estilo ao footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,75 @@
-<footer>
-    <br><br>
-    <p>© Copyright 2020</p>
-</footer>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
-</script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
-</script>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+
+
+    <footer>
+
+        <div class="faixinha">
+
+        </div>
+        <div class="contentaaa">
+            <div class="colher">
+                <h3><b>Sala DCE</b></h3>
+                <h6><b>Bloco B - 110</b> <br>
+                    <b> Ao lado do Xeróx</b>
+                </h6>
+            </div>
+        </div>
+        <div class="copyright">
+            <p>© Copyright 2020</p>
+        </div>
+        <div class="footer-1">
+
+
+            <div class="redes-sociais">
+                <ul>
+                    <li>
+                        <a href="https://www.facebook.com/DCEUFFS" target="_blank">
+                            <img src="./imagens/face.png" width="45" height="45" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href=" https://www.instagram.com/dceuffs/" target="_blank">
+                            <img src="./imagens/insta.png" width="45" height="45" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href=" https://www.instagram.com/dceuffs/" target="_blank">
+                            <img src="./imagens/twitter.png" width="45" height="45" />
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
