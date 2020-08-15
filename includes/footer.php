925e222a401a439422750b81e68b99f45593e2ba
Rafinha <arruda.rafa.ra@gmail.com>
2020-08-15 13:39:10 -0400

    Revert "ajeitando o header e adicionando paginas para fazer as rotas"

--- includes/footer.php
+++ includes/footer.php
@@ -1,58 +1,16 @@
+
     <footer>
+        <br><br>
+        <p>© Copyright 2020</p>
 
-        <div class="faixinha">
-
-        </div>
-        <div class="contentaaa">
-            <div class="colher">
-                <h3>Sala DCE</h3>
-                <h6>Bloco B - 110 <br>
-                    Ao lado do Xeróx
-                </h6>
-            </div>
-        </div>
-        <div class="copyright">
-            <p>© Copyright 2020</p>
-        </div>
-        <div>
-
-
-            <div class="redes-sociais">
-                <ul>
-                    <li>
-                        <a href="https://www.facebook.com/DCEUFFS" target="_blank">
-                            <img src="./imagens/face.png" width="45" height="45" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href=" https://www.instagram.com/dceuffs/" target="_blank">
-                            <img src="./imagens/insta.png" width="45" height="45" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href=" https://www.instagram.com/dceuffs/" target="_blank">
-                            <img src="./imagens/twitter.png" width="45" height="45" />
-                        </a>
-                    </li>
-
-                </ul>
-            </div>
-        </div>
     </footer>
 
 
 
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-
-
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
-    </script>
 </body>
-
 </html>
